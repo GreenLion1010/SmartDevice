88cd1aa415ce785ad612712d4ef5bd02a8aa2b28
inux <unknown-user@192.168.1.26>
2015-01-25 22:16:46 +0100

    Clean class name

---
+++ SmartDeviceDefs/IO/SystemInput.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace SmartDeviceDefs
+{
+	public class SystemInput
+	{
+		private bool _IsActive = false;
+
+		public SystemInput ()
+		{
+		}
+
+		public bool IsActive {
+			get {
+				return _IsActive;
+			}
+			set {
+				_IsActive = value;
+			}
+		}
+	}
+}
+
